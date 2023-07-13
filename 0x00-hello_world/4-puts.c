ff94899fba467c2ee16633afe31472a12ce37801
BNaledi <naledimohla16.com>
2023-07-13 15:53:47 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Print text using puts function
+ * Print text using puts instead of printf function
  *
  * Return: Always (0) (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
