405339a658efd315c17a1f9ae0cbeab3af86fd8f
BNaledi <naledimohla16.com>
2023-07-17 13:02:50 -0700

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,11 +12,11 @@ int main(void)
 {
 	int c;
 
-	for (c = 65; c <= 90; c++)
+	for (c = 97; c <= 122; c++)
 	{
 		putchar(c);
 	}
-	for (c = 97; c <= 122; c++)
+	for (c = 65; c <= 90; c++)
 	{
 		putchar(c);
 	}
