62836cd8838d7b72e863336ff6b60fcd00a9c190
BNaledi <naledimohla16.com>
2023-07-18 12:02:56 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Prints all numbers of base 16
+ * in lowercase,
+ * followed by a new line.
+ *
+ * Can only use putchar thrice in your code.
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int a = 30;
+
+	if (a <= 39)
+	{
+		putchar(a);
+		a++;
+	}
+	else if (a > 61 && a<67) 
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
