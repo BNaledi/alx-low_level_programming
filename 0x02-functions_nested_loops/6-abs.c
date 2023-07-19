ea79bfd37dc2ea3c81255d7c8d7ab840bf0266e4
BNaledi <naledimohla16.com>
2023-07-19 13:41:36 -0700

    6. There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,19 +7,19 @@
  *
  * @n: The int character that is converted.
  *
- * Return: 0 Always
- * if n is greater than, equal to zero,print n
+ * Return: n if n is greater than, equal to zero,
  * if n is less than zero, print abs(n).
  */
 int _abs(int n)
 {
 	if (n < 0)
 	{
-		_putchar(-n);
+		imaxabs(n);
+		printf("%jd", n);
 	}
 	else if (n == 0)
 	{
-		_putchar(n);
+		_putchar('0');
 	}
 	else if (n > 0)
 	{
