3ea2d54fe3c45f3ddde0a15ecec94d8ea00c4461
BNaledi <naledimohla16.com>
2023-07-19 08:05:34 -0700

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -20,14 +20,14 @@ int print_sign(int n)
 		_putchar(' ');
 		return (1);
 	}
-	else if (c = 0)
+	else if (n = 0)
 	{
 	       _putchar(0);
 	       _putchar(',');
 	       _putchar(' ');
 	       return (0);
 	}
-	else if (c < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
 		_putchar(',');
