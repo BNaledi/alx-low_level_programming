e1e41b8103913b9ddc94ba3ab90da3504a4f4154
BNaledi <naledimohla16.com>
2023-07-19 14:03:35 -0700

    6. There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -12,17 +12,14 @@
  */
 int _abs(int n)
 {
-	if (n > 0)
+	if (n > 0):
 	{
-		_putchar(n);
+		return (n);
 	}
-	if (n == 0)
+	else if (n == 0)
 	{
-		_putchar(n);
+		return (n);
 	}
-	else
-	{
-		n = -n;
-	}
-	return (0);
+	n = -n;
+	return (n);
 }
