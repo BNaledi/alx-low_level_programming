a1bd9347c3c0d124493f1395eb0f1c6c33a00ab5
BNaledi <naledimohla16.com>
2023-07-18 17:03:45 -0700

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,11 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet
+ *
+ *Write a function that prints the alphabet,
+ in lowercase, followed by a new line.
+ May only use _putchar, twice.
  *
  * Return: Always 0.
  */
