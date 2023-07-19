caf3b3b2b9aeccc738edf6656b9b30e4d80e62f6
BNaledi <naledimohla16.com>
2023-07-19 00:14:19 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prototype function
+ *
+ *Prints the lowercase alphabet,using only _putchar, twice.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	count = 0;
+
+	while (count <= 10)
+	{
+		int a = 97;
+
+		while (a <= 122)
+		{
+			putchar(a);
+			a++;
+		}
+		putchar('\n');
+		count++;
+	}
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -12,7 +12,7 @@
  */
 int main(void)
 {
-    print_alphabet();
+    print_alphabet_x10();
     return (0);
 }
 
