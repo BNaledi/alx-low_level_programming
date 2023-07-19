77be474bf3f34826c9a59170480025c7775339cf
BNaledi <naledimohla16.com>
2023-07-19 06:06:02 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -12,14 +12,14 @@ int _islower(int c)
 	if (c > 96 && c <= 122)
 	{
 		c++;
-		return (1);
 		_putchar('\n');
+		return (1);
 	}
 	else if (c > 64 && c <= 90)
 	{
 		c++;
-		return (0);
 		_putchar('\n');
+		return (0);
 	}
 	return (0);
 }
