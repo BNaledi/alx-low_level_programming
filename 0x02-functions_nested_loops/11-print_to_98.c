cf8785b23c0bb015d362e6605b8d4a7a9b50fffe
BNaledi <naledimohla16.com>
2023-07-19 17:08:03 -0700

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prototype function.
+ * 
+ * Prints all natural numbers from n to 98, followed by a new line.
+ *
+ * Numbers must be separated by a comma, followed by a space.
+ * Numbers should be printed in order.
+ * @n: The first printed number passed to your function.
+ * The last printed number should be 98.
+ * Return: nine times tsbles.
+ */
+void print_to_98(int n)
+{
+	int n;
+
+	for (;n <= 98; n++)
+	{
+		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+}
