5a56a6c2ad3151e162ff07c2d7fc4aefbe949c24
BNaledi <naledimohla16.com>
2023-07-20 15:18:47 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * is_upper - prototype function.
+ *
+ * Checks for uppercase character.
+ *
+ * @c: The character that needs to be checked
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,23 @@
+#ifndef MAIN_C
+#define MAIN_C
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
+}
+
+#endif
