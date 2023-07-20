e37e35474f56abbfbe373289d7a7eb2d522e1d98
BNaledi <naledimohla16.com>
2023-07-20 10:44:35 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 
 /**
  * print_to_98 - prototype function.
- *
+ * 
  * Prints all natural numbers from n to 98, followed by a new line.
  *
  * Numbers must be separated by a comma, followed by a space.
