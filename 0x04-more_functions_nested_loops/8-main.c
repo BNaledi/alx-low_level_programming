499b0d7e3bd70e9f66cc3077ae93e99fcdd0accf
BNaledi <naledimohla16.com>
2023-07-21 16:51:37 -0700

    8. You are so much sunshine in every square inch

---
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -0,0 +1,20 @@
+#ifndef MAIN_C
+#define MAIN_C
+
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_square(2);
+	print_square(10);
+	print_square(0);
+	return (0);
+}
+
+
+#endif
