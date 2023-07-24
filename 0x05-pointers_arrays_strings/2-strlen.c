db4c8565db0a2ec03bf6f37be972e87c68e7c28e
BNaledi <naledimohla16.com>
2023-07-24 14:51:44 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strlen - prototype function
+ *
+ * Returns the length of a string.
+ *
+ * @s: the pointer to string characters.
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	if (*str != '\0')
+	{
+		len++;
+		++str;
+	}
+	else
+	{
+		return (len);
+	}
+	return (len);
+}
