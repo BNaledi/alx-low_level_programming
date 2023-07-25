e5ffbea84b63e94a3ae7837bfad0df6070ad8211
BNaledi <naledimohla16.com>
2023-07-25 14:11:50 -0700

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - prototype function
+ *
+ * prints every other character of a string,
+ * starting with the first character,
+ * followed by a new line.
+ *
+ * @str: the pointer to string characters.
+ *
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int len = strlen(str);
+
+	if (len % 2 != 0)
+	{
+		while (*str != '\0')
+		{
+		_putchar(*str);
+		str++;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*str);
+	}
+}
