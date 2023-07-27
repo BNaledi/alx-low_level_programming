a6e912b913fe9a688c95cfa79cbb54e1a279cb4e
BNaledi <naledimohla16.com>
2023-07-27 15:39:58 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,23 +5,42 @@
  *
  * Function that concatenates two strings.
  *
+ * @dest: string 1
+ * @src: string 2
  * Return: a pointer to the resulting string dest.
  */
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
 {
 	int len;
-	int i;
 
 	for (len = 0; *dest != '\0'; len++)
 	{
-		*dest++;
+		dest++;
 	}
 	while (*src != '\0')
 	{
+/*
+ * At NULL for dest must begin src string)
+ */
 		*dest = *src;
-		_putchar(src[]);
-		*dest++;
-		*src++;
+/*
+ * As we increment the now dest = dest+src) we are ensuring
+ * more memory blocks to accom src len
+ */
+		dest++;
+/*
+ * At position opened up by dest+new block.
+ * We place src as we go until its end
+ */
+		src++;
+		len++;
 	}
-	_putchar('\n');
+/*
+ * Dereferencing dest to NULL at end, i.e. making it a variable,
+ * changed only last address pointer points to 1
+ * address at a time)
+ */
+	*dest = '\0';
+	dest = dest - len;/* return to dest[0] */
+	return (dest);
 }
