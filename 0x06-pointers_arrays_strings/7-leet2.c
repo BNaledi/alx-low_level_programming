1c4095b3f97c2f3a776ad492e91428147fa2aa13
BNaledi <naledimohla16.com>
2023-08-05 15:52:06 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet2.c
+++ 0x06-pointers_arrays_strings/7-leet2.c
@@ -8,7 +8,10 @@
  * Letters o and O should be replaced by 0
  * Letters t and T should be replaced by 7
  * Letters l and L should be replaced by 1
- * You can only use if once.
+ * You can only use one if in your code
+ * You can only use two loops in your code
+ * You are not allowed to use switch
+ * You are not allowed to use any ternary operation
  *
  * @n: input value, a string
  *
